e5a865356d1fba560055512ca3180d6caf564a58
Sophie <fi3zz0p@gmail.com>
2025-07-29 21:52:29 +0430

    Fix host binding, add gunicorn and timeout

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ def index():
     else:
         return render_template('index.html', schedule=[])
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    import os
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
